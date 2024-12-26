5f401bec8676444c3aa0ed358e2bc68ad20bd6e5
Antonenko Aleksandr <114353007+daates@users.noreply.github.com>
2024-12-26 23:09:51 +0300

    Update school_classes_controller.rb

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -1,14 +1,6 @@
 class SchoolClassesController < ApplicationController
   # GET /schools/:school_id/school_classes
-  def get_class_list
-    begin
-      @school = School.find(params[:school_id])
-      @school_classes = @school.school_classes
-      render json: { data: @school_classes }, status: :ok
-    rescue ActiveRecord::RecordNotFound
-      render json: { error: "School not found" }, status: :not_found
-    end
-  end
+  
 
 
   def get_class_students_list
