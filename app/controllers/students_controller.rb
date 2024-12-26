debdd54b8c96112644ee0d7ad84bdb82e2e6d55f
Antonenko Aleksandr <114353007+daates@users.noreply.github.com>
2024-12-26 23:07:41 +0300

    Update students_controller.rb

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,23 @@
 class StudentsController < ApplicationController
   before_action :authenticate_with_token!, only: [ :destroy ]
 
-  
+  def create
+    @student = Student.find_or_initialize_by(
+      first_name: student_params[:first_name],
+      last_name: student_params[:last_name],
+      surname: student_params[:surname],
+      school_id: student_params[:school_id],
+      school_class_id: student_params[:school_class_id]
+    )
+
+    @student.gen_token!
+
+    if @student.save
+      render json: @student, status: :created, headers: { 'X-Auth-Token': @student.auth_token }
+    else
+      render json: { errors: @student.errors.full_messages }, status: :unprocessable_content
+    end
+  end
 
 
   def destroy
